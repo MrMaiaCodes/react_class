c34fdfe49817f2f5f30e532d1d3f01d1d2f31b7d
MrMaia <mrmaiacodes@gmail.com>
2024-04-16 09:41:33 -0300

    imported HomePage into App.tsx

--- my-first-react-project/src/App.tsx
+++ my-first-react-project/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import HomePage from './pages/Home';
 import './App.css';
+import "."
 function App() {
   return (
     <div className="App">
-      
+      <HomePage></HomePage>
     </div>
   );
 }
-
+//HomePage
 export default App;

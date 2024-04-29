e63b35b282a17abebb4fe7a92ba0a68e0cd0ccb8
MrMaia <mrmaiacodes@gmail.com>
2024-04-29 20:21:19 -0300

    did header component

--- my-first-react-project/src/App.tsx
+++ my-first-react-project/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import HomePage from './pages/Home';
+import {Header} from './components/Header';
 import './App.css';
 function App() {
   return (
+
     <div className="App">
-      <HomePage></HomePage>
+      <HomePage>
+      
+      </HomePage>
     </div>
   );
 }

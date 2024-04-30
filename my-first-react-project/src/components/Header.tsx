4dd2e31b68e1e21ffabbc7b691fb6fc5f9c073d3
MrMaia <mrmaiacodes@gmail.com>
2024-04-30 14:17:32 -0300

    wrote header component

--- my-first-react-project/src/components/Header.tsx
+++ my-first-react-project/src/components/Header.tsx
@@ -1,19 +1,19 @@
-import { Button } from "./Button";
+import {Button} from "./Button";
 
 export const Header = (props: any) => {
     return (
         <div
-            style={{
-                backgroundColor: "red",
-                height: "60px"
-            }}
+        style={{
+            backgroundColor: "red",
+            height: "60px"
+        }}
         >
+
+            <Button text = {"test"} textColor = {"red"} onClickButton = {() => {}}/>
+            <Button text = {"home"} textColor = {"red"} onClickButton = {() => {}}/>
+            <Button text = {"about me"} textColor = {"red"} onClickButton = {() => {}}/>
+            <Button text = {"about the app"} textColor = {"red"} onClickButton = {() => {}}/>
             
-            <Button text={"test"} textColor={"red"} onClickButton={() => {}}/>
-                <Button text={"home"} textColor={"red"} onClickButton={() => {}}/>
-                <Button text={"about me"} textColor={"red"} onClickButton={() => {}}/>
-                <Button text={"about the app"} textColor={"red"} onClickButton={() => {}}/>
-        
         </div>
     )
 };

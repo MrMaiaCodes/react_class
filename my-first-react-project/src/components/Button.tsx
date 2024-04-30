0ad87af6290b8008a798558e1e681a25621b103a
MrMaia <mrmaiacodes@gmail.com>
2024-04-30 14:04:37 -0300

    Button tab

---
+++ my-first-react-project/src/components/Button.tsx
@@ -0,0 +1,20 @@
+interface ButtonTemplateProps{
+    textColor: string;
+    text: string;
+    onClickButton: ()=> void;
+}
+export const Button = (props: ButtonTemplateProps) => {
+    const {text} = props;
+    return (
+        <button style = {{
+            color: props.textColor,
+            background: "white",
+            height: "40px",
+            width: "100px",
+            borderRadius: "20px",
+            margin: "8px"
+        }}>
+            {text}
+        </button>
+    )
+}

15c3917ddd7d3230c4d6d8a1b35739bb12f2dcc5
MrMaia <mrmaiacodes@gmail.com>
2024-05-02 21:47:19 -0300

    redid Home

--- my-first-react-project/src/components/Header.tsx
+++ my-first-react-project/src/components/Header.tsx
@@ -1,24 +1,3 @@
-/**
- * import {Button} from "./Button";
-
-export const Header = (props: any) => {
-    return (
-        <div
-        style={{
-            backgroundColor: "red",
-            height: "60px"
-        }}
-        >
-
-            <Button text = {"test"} textColor = {"red"} onClickButton = {() => {}}/>
-            <Button text = {"home"} textColor = {"red"} onClickButton = {() => {}}/>
-            <Button text = {"about me"} textColor = {"red"} onClickButton = {() => {}}/>
-            <Button text = {"about the app"} textColor = {"red"} onClickButton = {() => {}}/>
-            
-        </div>
-    )
-};
- */
 import {Button} from "./Button";
 
 export const Header = (props: any) => {
@@ -29,10 +8,10 @@ export const Header = (props: any) => {
             height: "60px"
         }}
         >
-            <Button text = {"test"} textColor = {"red"} onClickButton = {{} => {}}/>
-            <Button text = {"home"} textColor = {"red"} onClickButton = {{} => {}}/>
-            <Button text = {"about me"} textColor = {"red"} onClickButton = {{} => {}}/>
-            <Button text = {"about the app"} textColor = {"red"} onClickButton = {{} => {}}/>
+            <Button text = {"test"} textColor = {"red"} onClickButton = {() => {}}/>
+            <Button text = {"home"} textColor = {"red"} onClickButton = {() => {}}/>
+            <Button text = {"about me"} textColor = {"red"} onClickButton = {() => {}}/>
+            <Button text = {"about the app"} textColor = {"red"} onClickButton = {() => {}}/>
         </div>
     )
-}
+};
--- my-first-react-project/src/pages/Home.tsx
+++ my-first-react-project/src/pages/Home.tsx
@@ -1,10 +1,11 @@
+
+
 import {useState} from "react";
 import {Header} from "../components/Header"
 export default function Homepage(props: any) {
    return(
       <div style = {{}}>
          <Header>
-
 
          </Header>
       </div>
